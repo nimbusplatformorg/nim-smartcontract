8267dca709525c7a171bb436af73d630251306e1
Paul Zibarov <paul.zibarov@gmail.com>
2021-04-26 16:38:41 +0300

    Enabled compile optimizer in truffle-config. Add build folder to .gitignore.

--- truffle-config.js
+++ truffle-config.js
@@ -45,17 +45,19 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "*",       // Any network (default: none)'
+     gasPrice: 0x1,
+     gas: 0x1fffffffffffff
     },
     // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
+    //advanced: {
+      //port: 8777,             // Custom port
+      //network_id: 1342,       // Custom network
+      //gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+      //gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      //from: <address>,        // Account to send txs from (default: accounts[0])
+      //websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    //},
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
@@ -84,13 +86,13 @@ module.exports = {
     solc: {
       version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 1000
+       },
       //  evmVersion: "byzantium"
-      // }
+      }
     }
   },
 
