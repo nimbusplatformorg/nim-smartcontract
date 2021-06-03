b8f3ed81c2311b9baca4241323a727a5c4ff739e
NikolasTS <nikolastsarev@gmail.com>
2021-06-03 10:59:01 +0300

    InitialAcquisition tests

--- test/utils/constants.js
+++ test/utils/constants.js
@@ -1,8 +1,9 @@
-const { BN } = require("@openzeppelin/test-helpers");
-const ZERO = new BN(0);
-const DAY = new BN("86400");
+const { BN, constants } = require("@openzeppelin/test-helpers");
 
 module.exports = {
-  ZERO,
-  DAY,
+  ZERO: new BN(0),
+  DAY: new BN("86400"),
+  ONE_ADDRESS: "0x0000000000000000000000000000000000000001",
+  ZERO_BYTES: Buffer.from(""),
+  ...constants,
 };
