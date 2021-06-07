aee79d46cf6197caf2a934de54b5463793ca9869
McKey <mail@mail.com>
2021-06-07 20:48:01 +0300

    Rename contracts

--- contracts/contracts_BSC/dApps/RevenueChannels/interfaces/IBEP20.sol
+++ contracts/contracts_BSC/dApps/RevenueChannels/interfaces/IBEP20.sol
@@ -6,11 +6,14 @@ contract IBEP20 {
     uint8 public decimals;
     string public symbol;
     function totalSupply() public view returns (uint256);
+    function decimals() external view returns (uint8);
     function balanceOf(address _who) public view returns (uint256);
     function allowance(address _owner, address _spender) public view returns (uint256);
     function approve(address _spender, uint256 _value) public returns (bool);
     function transfer(address _to, uint256 _value) public returns (bool);
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
+    function getOwner() external view returns (address);
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
