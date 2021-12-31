1386c73565a0ff9eb82f13a73743980c845007df
Maxim280596 <max280596@gmail.com>
2021-12-31 14:03:57 +0200

    fix require

--- contracts/contracts_BSC/dApps/NFTTokens/SmartLender.sol
+++ contracts/contracts_BSC/dApps/NFTTokens/SmartLender.sol
@@ -591,6 +591,9 @@ contract SmartLP is SmartLPStorage, IBEP721, IBEP721Metadata {
     function getTokenRewardsAmounts(uint tokenId) public view returns (uint lpBnbNbuUserRewards, uint lpBnbGnbuUserRewards, uint lendedUserRewards) {
         UserSupply memory userSupply = tikSupplies[tokenId];
         require(userSupply.IsActive, "SmartLP: Not active");
+        require(userSupply.LendedBNBAmount <= ((userSupply.LendedITokenAmount *lendingContract.tokenPrice()) / 1e18),
+            "SmartLP: lending rewards are not available"
+        );
         lpBnbNbuUserRewards = (_balancesRewardEquivalentBnbNbu[tokenId] * ((block.timestamp - weightedStakeDate[tokenId]) * 100)) / (100 * rewardDuration);
         lpBnbGnbuUserRewards = (_balancesRewardEquivalentBnbGnbu[tokenId] * ((block.timestamp - weightedStakeDate[tokenId]) * 100)) / (100 * rewardDuration);
         lendedUserRewards = (userSupply.LendedITokenAmount * lendingContract.tokenPrice() / 1e18) - userSupply.LendedBNBAmount;
